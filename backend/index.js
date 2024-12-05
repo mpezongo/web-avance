9809bc243a5150f6cae68d4bcee6e1a55e6d63db
Pezongo Mickael <pezongomickael67@gmail.com>
2024-12-05 12:00:12 +0100

    ajout de la fonctionnalites de produits favoris dans le backend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const db = require("./models/db")
 const usersRouter = require('./routes/users')
 const productsRouter = require('./routes/product')
 const cartRouter = require('./routes/cart')
+const favoriteRouter = require('./routes/favorites')
 
 const app = express()
 app.use(cookieParser());
@@ -26,9 +27,11 @@ app.get('/', (req, res) => {
   res.send("Bonjour le serveur est actif")
 })
 
-  app.use("/users", usersRouter)
-  app.use('/products', productsRouter)
-  app.use('/cart', cartRouter)
+app.use("/users", usersRouter)
+app.use('/products', productsRouter)
+app.use('/cart', cartRouter)
+app.use('/favorite', favoriteRouter)
+
 
 app.listen(5000, () => {
     console.log("server is start")
